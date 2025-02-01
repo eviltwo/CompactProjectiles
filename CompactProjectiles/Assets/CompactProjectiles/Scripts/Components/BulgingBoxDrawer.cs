aaabd8577073e84a1be7e7690e7e0f129e5536fc
DAIKI <daiki.evilone@gmail.com>
2025-02-01 19:16:01 +0900

    Move drawing box func to utility.

--- CompactProjectiles/Assets/CompactProjectiles/Scripts/Components/BulgingBoxDrawer.cs
+++ CompactProjectiles/Assets/CompactProjectiles/Scripts/Components/BulgingBoxDrawer.cs
@@ -41,44 +41,13 @@ namespace CompactProjectiles
             }
         }
 
+        private Color _gizmosColor = new Color(1, 1, 1, 0.5f);
+
         private void OnDrawGizmos()
         {
             var box = BulgingBox;
-
-            for (int d = 0; d < 3; d++)
-            {
-                var dir = Vector3.zero;
-                dir[d] = 1f;
-                DrawFaceGizmos(box, d, dir, BulgingBox.LocalSphereRadius);
-            }
-        }
-
-        private void DrawFaceGizmos(BulgingBox box, int dIndex, Vector3 localDirection, float radius)
-        {
-            var d1 = (dIndex + 1) % 3;
-            var d2 = (dIndex + 2) % 3;
-            for (int i = 0; i < LineCount; i++)
-            {
-                var ti = (float)(i + 1) / (LineCount + 1);
-                var scale = Mathf.Cos(ti * Mathf.PI) * radius;
-                var dir = localDirection * scale;
-                var r = Mathf.Sin(ti * Mathf.PI) * radius;
-                var sectionCount = (LineCount + 1) * 4;
-                for (int j = 0; j < sectionCount; j++)
-                {
-                    var tj = (float)j / sectionCount;
-                    var rad = tj * Mathf.PI * 2;
-                    var p1 = dir;
-                    p1[d1] = Mathf.Cos(rad) * r;
-                    p1[d2] = Mathf.Sin(rad) * r;
-                    tj = (float)(j + 1) / sectionCount;
-                    rad = tj * Mathf.PI * 2;
-                    var p2 = dir;
-                    p2[d1] = Mathf.Cos(rad) * r;
-                    p2[d2] = Mathf.Sin(rad) * r;
-                    Gizmos.DrawLine(box.SphereToWorldPoint(p1), box.SphereToWorldPoint(p2));
-                }
-            }
+            Gizmos.color = _gizmosColor;
+            BulgingBoxUtility.DrawBoxGizmos(box, LineCount);
         }
     }
 }
